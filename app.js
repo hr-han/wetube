07b21f89f8dd27e71e756688e6d06d86c878757f
hr-han <hr-han@users.noreply.github.com>
2020-02-24 22:48:17 +0900

    set pug

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import routes from "./routes"
 
 const app = express();
 
+app.set('view engine', "pug")
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,7 +18,7 @@ app.use(helmet());
 app.use(logger("dev"));
 
 
-app.use(routes.home, globalRouter);
+app.use(routes.home, globalRouter,userRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
